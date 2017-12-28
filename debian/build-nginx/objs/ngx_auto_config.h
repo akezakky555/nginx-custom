de033dba6929dd5cc96903c31b9f57800a07fbbf
Zakky <ake.zakky@gmail.com>
2017-12-28 06:05:51 +0000

    Add file

---
+++ debian/build-nginx/objs/ngx_auto_config.h
@@ -0,0 +1,14 @@
+#define NGX_CONFIGURE " --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --add-module=/usr/local/src/nginx-1.13.8/debian/modules/headers-more-nginx-module --add-module=/usr/local/src/nginx-1.13.8/debian/modules/ngx_brotli --add-module=/usr/local/src/nginx-1.13.8/debian/modules/ngx_cache_purge-2.3 --add-module=/usr/local/src/nginx-1.13.8/debian/modules/ngx_pagespeed-1.12.34.3-stable --with-openssl=/usr/local/src/nginx-1.13.8/debian/modules/boringssl --with-cc-opt= --with-ld-opt="
+
+#ifndef NGX_HAVE_NONALIGNED
+#define NGX_HAVE_NONALIGNED  1
+#endif
+
+
+#ifndef NGX_CPU_CACHE_LINE
+#define NGX_CPU_CACHE_LINE  64
+#endif
+
+
+#define NGX_KQUEUE_UDATA_T  (void *)
+
